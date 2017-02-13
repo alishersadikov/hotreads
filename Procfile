986eaec11a50fed6356c815293ab6483ee383664
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-13 16:09:13 -0700

    add consumer url

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+sneaker: WORKERS=LinksWorker bundle exec rake sneakers:run
--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,2 +1,7 @@
 Sneakers.configure({})
+require 'sneakers'
+Sneakers.configure  amqp: ENV['RABBITMQ_CONSUMER'],
+                    threads: 1,
+                    workers: 1
+
 Sneakers.logger.level = Logger::INFO # the default DEBUG is too noisy
